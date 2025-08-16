a85538df869751852526edd7076c00b0368114e7
Juan Manuel Sánchez Martagón <jmsanchezmartagon@gmail.com>
2025-08-16 22:06:16 +0200

    wip. multi module for openapi

---
+++ product-store-app/src/main/java/es/jambo/product_store/product/price_finder/infrastructure/entrypoint/api/BrandProductPriceAPIImpl.java
@@ -0,0 +1,8 @@
+package es.jambo.product_store.product.price_finder.infrastructure.entrypoint.api;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BrandProductPriceAPIImpl {
+
+}
